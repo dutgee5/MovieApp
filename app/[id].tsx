ea6b654c2e26e31417db3e0cc16c57ec6b59f857
theDone <done.muzaffer5@gmail.com>
2024-07-07 17:00:32 +0300

    fetch movie details

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,16 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useLocalSearchParams } from "expo-router";
+import { useQuery } from "@tanstack/react-query";
+import { fetchMovieDetails } from "@/api/movie";
 
 const MovieDetails = () => {
-    const {id}= useLocalSearchParams();
+  const { id } = useLocalSearchParams();
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["movies", id],
+    queryFn: () => fetchMovieDetails(id),
+  });
+
+  if (isLoading) return <ActivityIndicator />;
+
+  if (error) return <Text>{error.message}</Text>;
   return (
     <View>
-      <Text>{id}</Text>
+      <Text>{data.title}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default MovieDetails
+export default MovieDetails;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
